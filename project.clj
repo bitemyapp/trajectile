5367e169c54a950b3cdfa1dba81f2d8e44ca4864
Chris Allen <cma@bitemyapp.com>
2013-08-03 12:09:19 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject trajectile "0.0.1"
+  :description "Performance tracing and logging library"
+  :url "http://github.com/bitemyapp/trajectile"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.taoensso/timbre "2.4.1"]])
---
+++ test/trajectile/core_test.clj
@@ -0,0 +1,7 @@
+(ns trajectile.core-test
+  (:require [clojure.test :refer :all]
+            [trajectile.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

2d22483b7e97845169bd3f35759044639ac9364e
Chris Allen <cma@bitemyapp.com>
2013-08-03 21:21:15 -0700

    might be some warm-up involved to the overhead. Willing to call it good for now.

--- test/trajectile/core_test.clj
+++ test/trajectile/core_test.clj
@@ -4,11 +4,22 @@
 
 (defn sleep-and-return [time]
   (trace :begin)
-  (Thread/sleep 100)
+  (Thread/sleep time)
   (trace :end))
+
+(defn sleep-twice-and-return [time]
+  (trace :begin)
+  (Thread/sleep time)
+  (Thread/sleep time)
+  (trace :end))
+
 
 (deftest trace-sleep
   (testing "We see accurate numbers for sleeps"
     (is (= true
            (let [time-returned (sleep-and-return 100)]
-             (and (> time-returned 99) (< time-returned 115)))))))
+             (and (> time-returned 99) (< time-returned 115)))))
+    (is
+        (= true
+           (let [time-returned (sleep-twice-and-return 100)]
+             (and (> time-returned 199) (< time-returned 230)))))))
